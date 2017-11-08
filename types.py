2e384aca18028baf0b0b27288039579cde743467
kageru <kageru@encode.moe>
2017-11-08 19:44:51 +0100

    added __repr__ to message

--- types.py
+++ types.py
@@ -8,9 +8,18 @@ class Interval():
 class Message():
     text: str
     author: str
+    author_mode: str
+    channel: str
     timestamp: datetime
 
-    def __init__(self, text, author, year, month, day, hour, minute, second):
+    def __init__(self, text, author, year, month, day, hour, minute, second, channel, author_mode=None):
         Message.timestamp = datetime(year, month, day, hour, minute, second)
         Message.text = text
         Message.author = author
+        Message.channel = channel
+        Message.author_mode = author_mode if author_mode is not None else ''
+
+    def __repr__(self):
+        return f'In #{self.channel} on [{self.timestamp.year}/{self.timestamp.month}/{self.timestamp.day}] ' + \
+               'at [{:02d}:{:02d}:{:02d}]'.format(self.timestamp.hour, self.timestamp.minute, self.timestamp.second) + \
+               f' <{self.author_mode}{self.author}> {self.text}'
